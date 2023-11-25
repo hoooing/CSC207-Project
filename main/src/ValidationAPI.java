d80721b49be3bd7162d95c15553f27510622c74f
ProudScottsTot <87554004+ProudScottsTot@users.noreply.github.com>
2023-11-25 16:40:57 -0500

    edit

--- main/src/ValidationAPI.java
+++ main/src/ValidationAPI.java
@@ -11,7 +11,7 @@ public class ValidationAPI {
 
         try {
 
-            Content content = Request.Get('https://emailvalidation.abstractapi.com/v1/?api_key=7190df0be70043ee94cd825f023b8de6&email=eocks0615@gmail.com')
+            Content content = Request.Get("https://emailvalidation.abstractapi.com/v1/?api_key=7190df0be70043ee94cd825f023b8de6&email=eocks0615@gmail.com")
 
                     .execute().returnContent();
 
