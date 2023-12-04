4d85d57d6b555e6eb0edc1c716c924ebd5af0c02
daechan eom <d.eom@mail.utoronto.ca>
2023-12-04 06:50:47 -0500

    Implemented Add Friend further.

--- main/src/interface_adapter/add_friend/AddFriendPresenter.java
+++ main/src/interface_adapter/add_friend/AddFriendPresenter.java
@@ -5,16 +5,29 @@ import use_case.add_friend.AddFriendOutputData;
 
 public class AddFriendPresenter implements AddFriendOutputBoundary {
 
-//    private final AddFriendViewModel addFriendViewModel;
+    private final AddFriendViewModel addFriendViewModel;
 //    private
+
+    public AddFriendPresenter(AddFriendViewModel viewModel) {
+        this.addFriendViewModel = viewModel;
+    }
 
     @Override
     public void prepareSuccessView(AddFriendOutputData user) {
+        AddFriendState state = addFriendViewModel.getState();
+        String time = user.getAddedTime();
+        String usernameAdded = user.getFriendName();
+        String message = "User " + usernameAdded + " successfully deleted at " + time;
 
+        state.setMessage(message);
+        addFriendViewModel.firePropertyChanged();
     }
 
     @Override
     public void prepareFailView(String errorMessage) {
+        AddFriendState state = addFriendViewModel.getState();
+        state.setMessageError(errorMessage);
+        addFriendViewModel.firePropertyChanged();
 
     }
 }
--- main/src/interface_adapter/add_friend/AddFriendState.java
+++ main/src/interface_adapter/add_friend/AddFriendState.java
@@ -2,7 +2,29 @@ package interface_adapter.add_friend;
 
 public class AddFriendState {
 
-    public AddFriendState() {
+    private String message = "";
+    private String messageError = null;
+
+    public AddFriendState(AddFriendState copy) {
+        this.message = copy.message;
+        this.messageError = copy.messageError;
     }
 
+    public AddFriendState() {}
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getMessageError() {
+        return messageError;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setMessageError(String messageError) {
+        this.messageError = messageError;
+    }
 }
