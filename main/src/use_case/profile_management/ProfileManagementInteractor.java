f84dba26bfab9aa4e1bb849d045c285983025ae6
hoooing <kdh010125@gmail.com>
2023-12-07 23:50:47 -0500

    partially implemented Interactor and DataAccessInterface.

---
+++ main/src/use_case/profile_management/ProfileManagementInteractor.java
@@ -0,0 +1,34 @@
+package use_case.profile_management;
+
+import entity.User;
+
+public class ProfileManagementInteractor implements ProfileManagementInputBoundary {
+
+    private final ProfileManagementUserDataAccessInterface userDataAccessInterface;
+
+    public ProfileManagementInteractor(ProfileManagementUserDataAccessInterface userDataAccessInterface) {
+        this.userDataAccessInterface = userDataAccessInterface;
+    }
+
+    @Override
+    public void updateProfile(ProfileManagementInputData inputData) {
+        User user = userDataAccessInterface.getUser(inputData.getUsername());
+        if (user != null) {
+            // Update user details
+            user.setPassword(inputData.getNewPassword());
+            user.setEmail(inputData.getNewEmail());
+            user.setProfilePicture(inputData.getProfilePicture());
+            userDataAccessInterface.updateUser(user);
+        }
+        // Handle the case when user is not found
+    }
+
+    @Override
+    public void searchUser(String username) {
+        User user = userDataAccessInterface.getUser(username);
+        if (user != null) {
+            // Return or process the found user
+        }
+        // Handle the case when user is not found
+    }
+}
